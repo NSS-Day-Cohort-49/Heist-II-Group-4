c8c65b08b3d6a73840cf33e9e7d1e8521faa58de
Joel Beckum <joelbeckum@gmail.com>
2021-09-08 09:34:45 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeistTeamFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
